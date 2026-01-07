03d19cdcef8d2dcac2b40a375953e6980a8ffa55
Eder Gutierrez <gutierrezeder64@gmail.com>
2026-01-07 16:34:00 -0600

    Created text_node_to_html_node method which convert a TextNode to an LeafNode of html, considering all types sopported by md

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -3,7 +3,7 @@ Module used to test the TextNode class on different edge cases
 on the properties and the text types
 """
 import unittest
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq_nodes(self):
@@ -36,6 +36,48 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(
             "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
         )
+
+   #Testing text_node_to_html_node function
+    def test_textnode_to_htmlnode_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+    
+    def test_textnode_to_htmlnode_bold(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_textnode_to_htmlnode_italic(self):
+        node = TextNode("This is a text node", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This is a text node")
+    
+    def test_textnode_to_htmlnode_code(self):
+        node = TextNode("This is a text node", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "This is a text node")
+    
+    def test_textnode_to_htmlnode_link(self):
+        node = TextNode("This is a text node", TextType.LINK, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is a text node")
+        self.assertEqual(html_node.properties_to_html(), ' href="https://www.boot.dev"')
+    
+    def test_textnode_to_htmlnode_image(self):
+        node = TextNode("This is a image node", TextType.IMAGE, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.properties_to_html(), ' src="https://www.boot.dev" alt="This is a image node"')
+    
+    def test_textnode_to_htmlnode_invalid(self):
+        self.assertRaises(Exception, lambda: text_node_to_html_node(TextNode("This is a text node", TextType.UNKNOWN)))
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -4,6 +4,7 @@ inline text used in md text. This is to store the information of every
 line to turn them to HTML
 """
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
     TEXT = "text"
@@ -31,4 +32,23 @@ class TextNode:
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
     
+def text_node_to_html_node(text_node):
+    text_node_value = text_node.text
 
+    match (text_node.text_type):
+        case TextType.TEXT:
+            return LeafNode(None, text_node_value)
+        case TextType.BOLD:
+            return LeafNode("b", text_node_value)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node_value)
+        case TextType.CODE:
+            return LeafNode("code", text_node_value)
+        case TextType.LINK:
+            return LeafNode("a",text_node_value, {"href": text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img", "",
+                    {"src": text_node.url,
+                    "alt": text_node_value})
+        case _:
+            ValueError(f"invalid text type: {text_node.text_type}")
