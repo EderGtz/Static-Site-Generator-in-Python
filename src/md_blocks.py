18178f2e6e57d07cc78ae3c5def5462f0cc13237
Eder Gutierrez <gutierrezeder64@gmail.com>
2026-01-09 22:19:21 -0600

    Created block_to_block_type, which takes a md string and returns the type of md block it is

--- src/md_blocks.py
+++ src/md_blocks.py
@@ -1,17 +1,77 @@
 """This module works over inputs of md documents, and returns lists
 of blocks strings
 """
+from enum import Enum
+
+#The specific types of md blocks admited
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered list"
 
 #Takes a document and splitted it into blocks of strings
 def markdown_to_blocks(text):
-    text_splitted = text.split("\n\n")
-    new_text = []
-    for element in text_splitted:
-        element = element.strip()
-        if element == "":
+    blocks_splitted = text.split("\n\n")
+    filtered_blocks = []
+    for block in blocks_splitted:
+        block = block.strip()
+        if block == "":
             continue
-        lines = element.split("\n")
-        #Used loop comprehension to ommit inline spaces
-        cleaned = "\n".join(line.strip() for line in lines)
-        new_text.append(cleaned)
-    return new_text
+        filtered_blocks.append(block)
+    return filtered_blocks
+
+#Returns the type of block that was introduced
+def block_to_block_type(md_block):
+    #heading
+    if md_block.startswith("#"):
+        heading_counter = 0
+        for i in range(len(md_block)):
+            if md_block[i] == "#":
+                heading_counter += 1
+                continue
+            if md_block[i] == " " and 1 <= heading_counter <= 6:
+                return BlockType.HEADING
+            #Avoiding to loop over the whole string if the first non-#
+            #character is not a space
+            break
+    #code
+    if md_block.startswith("```\n") and md_block.endswith("```"):
+        return BlockType.CODE
+    #quote
+    block_splitted = md_block.split("\n")
+    is_valid = True
+    for line in block_splitted:
+        if line == "":
+            continue
+        if not line.startswith("> "):
+            is_valid = False
+            break
+    if is_valid is True:
+        return BlockType.QUOTE
+    #unordered list
+    is_valid = True
+    for line in block_splitted:
+        if line == "":
+            continue
+        if not line.startswith("- "):
+            is_valid = False
+            break
+    if is_valid is True:
+        return BlockType.UNORDERED_LIST
+    #ordered list
+    is_valid = True
+    counter = 0
+    for line in block_splitted:
+        if line == "":
+            continue
+        counter += 1
+        if not line.startswith(f"{counter}. "):
+            is_valid = False
+            break
+    if is_valid is True:
+        return BlockType.ORDERED_LIST
+    #If none of above are met, is a normal paragraph
+    return BlockType.PARAGRAPH
--- src/test_md_blocks.py
+++ src/test_md_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from md_blocks import markdown_to_blocks
+from md_blocks import BlockType, markdown_to_blocks, block_to_block_type
 
 class TestMDBlocks(unittest.TestCase):
 
@@ -53,8 +53,8 @@ This is the same paragraph on a new line
        This is another paragraph with _italic_ text and `code` here
 This is the same paragraph on a new line 
 
-- This is a list     
-- with items    
+- This is a list
+- with items
 """
         blocks = markdown_to_blocks(md)
         self.assertEqual(
@@ -65,6 +65,71 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+    #testing block type converter
+    def test_block_to_block_type_paragraph(self):
+        text = "Hello there"
+        self.assertEqual(block_to_block_type(text), BlockType.PARAGRAPH)
+    
+    def test_block_to_block_type_heading(self):
+        text = "####### Hello there"
+        self.assertNotEqual(block_to_block_type(text), BlockType.HEADING)
+    
+    def test_block_to_block_type_heading_2(self):
+        text = "###### Hello there"
+        self.assertEqual(block_to_block_type(text), BlockType.HEADING)
+    
+    def test_block_to_block_type_heading_3(self):
+        text = "######Hello there"
+        self.assertNotEqual(block_to_block_type(text), BlockType.HEADING)
+    
+    def test_block_to_block_type_code(self):
+        text = """```
+        Hello there
+        ```"""
+        self.assertEqual(block_to_block_type(text), BlockType.CODE)
+    
+    def test_block_to_block_type_code_2(self):
+        text = """```Hello there
+        ```"""
+        self.assertNotEqual(block_to_block_type(text), BlockType.CODE)
+    
+    def test_block_to_block_type_code_3(self):
+        text = """```
+        Hello there
+        ``"""
+        self.assertNotEqual(block_to_block_type(text), BlockType.CODE)
+    
+    def test_block_to_block_type_quote(self):
+        text = """
+> There was a time
+> When I could sleep at 8 at clock every knight"""
+        self.assertEqual(block_to_block_type(text), BlockType.QUOTE)
+    
+    def test_block_to_block_type_quote_2(self):
+        text = """> There was a time
+> When I could sleep at 8 at clock every knight"""
+        self.assertEqual(block_to_block_type(text), BlockType.QUOTE)
+    
+    def test_block_to_block_type_quote_3(self):
+        text = """>There was a time
+> When I could sleep at 8 at clock every knight"""
+        self.assertNotEqual(block_to_block_type(text), BlockType.QUOTE)
+    
+    def test_block_to_block_type_ul(self):
+        text = """
+- Eggs
+- Bananas
+- Fruits"""
+        self.assertEqual(block_to_block_type(text), BlockType.UNORDERED_LIST)
+
+    def test_block_to_block_type_ol(self):
+        text = """
+1. Eggs
+2. Bananas
+3. Fruits"""
+        self.assertEqual(block_to_block_type(text), BlockType.ORDERED_LIST)
+
+
 
 if __name__ == "__main__":
     unittest.main()
