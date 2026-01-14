c755cdc78dd81bbac595f6fbfba97868992fcc48
Eder Gutierrez <gutierrezeder64@gmail.com>
2026-01-13 20:42:00 -0600

    feat: implement recursive static directory synchronization
    - Added recursive logic to mirror the static directory into the public folder.
    - Separated initial cleanup from the recursive loop to prevent data loss.
    - Used os and shutil modules for robust file system operations.

---
+++ src/copy_static_to_public.py
@@ -0,0 +1,18 @@
+#This module contains a recursive function that copies all the contents
+#from a source directory to a dest directory. In this case, is used
+#to recreate the public dir of the ssg
+import os
+import shutil
+
+#Takes two paths: the source path that will be entirely copied, and a dest path to store it
+def copy_static_dir(source_path_dir, dest_path_dir):
+    #This line is util for every recursion call
+    if not os.path.exists(dest_path_dir):
+        os.mkdir(dest_path_dir)
+    for file in os.listdir(source_path_dir):
+        source_file = os.path.join(source_path_dir, file)
+        dest_path = os.path.join(dest_path_dir, file)
+        if os.path.isfile(source_file):
+            shutil.copy(source_file, dest_path)
+        else:
+            copy_static_dir(source_file, dest_path)
--- src/main.py
+++ src/main.py
@@ -1,6 +1,15 @@
-from textnode import TextType, TextNode
+import os
+import shutil
+from copy_static_to_public import copy_static_dir
+
+dir_path_static = "./static"
+dir_path_public = "./public"
 
 def main():
-    text_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(text_node)
-main()
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+    os.mkdir(dir_path_public)
+    copy_static_dir(dir_path_static, dir_path_public)
+
+if __name__ == "__main__":
+    main()
